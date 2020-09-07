89e1085c06a0b6035f84d2c0e8299a9c19ab7f5d
Marius Høgli Aasarød <44798981+marhaasa@users.noreply.github.com>
2020-09-07 22:32:55 +0200

    add fetch all result URLs

--- Cristin.py
+++ Cristin.py
@@ -7,14 +7,14 @@ from pandas.io.json import json_normalize
 ### Set desired institution ###
 institution = 7548
 
-
 done = False
 counter = 1
 thisPage = requests.get(f"https://api.cristin.no/v2/results?institution={institution}&per_page=1000")
 sys.stdout.write("Working")
+totalHits = thisPage.headers["X-Total-Count"]
 
-rawData = json.loads(thisPage.text)
-data = json_normalize(rawData)
+flatData = json_normalize(json.loads(thisPage.text))
+cleanData = flatData[["url"]]
 
 while not done:
     nextPage = requests.get(thisPage.links["next"]["url"])
@@ -24,13 +24,24 @@ while not done:
             counter = counter + 1
             sys.stdout.flush()
             sys.stdout.write(".")
-            rawData = json.loads(thisPage.text)
-            data = data.append(json_normalize(rawData), sort=True)
+            flatData = flatData.append(json_normalize(json.loads(thisPage.text)), sort=True)
+            cleanData = flatData[["url"]]
     except KeyError:
-        rawData = json.loads(nextPage.text)
-        data = data.append(json_normalize(rawData), sort=True)
+        flatData = flatData.append(json_normalize(json.loads(nextPage.text)), sort=True)
+        cleanData = flatData[["url"]]
         counter = counter + 1
         sys.stdout.write(" Done!")
-        print(f"\nThe last page of the API call is: https://api.cristin.no/v2/results?institution={institution}&page={counter}&per_page=1000")
         done = True
-data.to_excel("Publications.xlsx", index=False)
+
+### Check if all data is gathered ###
+totalHits = int(totalHits) + 1
+if len(cleanData.index) == int(totalHits):
+    print("\n=== All data gathered ===")
+    
+    ### Save to Publications.xlsx ###
+    cleanData.to_excel("Publications.xlsx", index=False)
+    print("=== Publications.xlsx written ===")
+else:
+        print("\n### ERROR WHILE FETCHING DATA ###")
+
+
