dc2580fe4e6174755460c90cf8a1b7209e3b430a
Marius Høgli Aasarød <44798981+marhaasa@users.noreply.github.com>
2020-09-07 14:31:23 +0200

    edit url

--- Cristin.py
+++ Cristin.py
@@ -20,18 +20,17 @@ while not done:
     nextPage = requests.get(thisPage.links["next"]["url"])
     try:
         if nextPage.links["next"] is not None:
-            if nextPage.links["next"]:
-                thisPage = nextPage
-                counter = counter + 1
-                sys.stdout.flush()
-                sys.stdout.write(".")
-                rawData = json.loads(thisPage.text)
-                data = data.append(json_normalize(rawData), sort=True)
+            thisPage = nextPage
+            counter = counter + 1
+            sys.stdout.flush()
+            sys.stdout.write(".")
+            rawData = json.loads(thisPage.text)
+            data = data.append(json_normalize(rawData), sort=True)
     except KeyError:
         rawData = json.loads(nextPage.text)
         data = data.append(json_normalize(rawData), sort=True)
         counter = counter + 1
         sys.stdout.write(" Done!")
-        print(f"\nThe last page of the API call is: https://api.cristin.no/v2/results?institution=7gi548&page={counter}&per_page=1000")
+        print(f"\nThe last page of the API call is: https://api.cristin.no/v2/results?institution={institution}&page={counter}&per_page=1000")
         done = True
 data.to_excel("Publications.xlsx", index=False)
