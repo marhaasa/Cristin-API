bdc0cff368fae0a7f602ec425992cbed3c3ec1ee
Marius Høgli Aasarød <44798981+marhaasa@users.noreply.github.com>
2020-09-08 12:16:19 +0200

    add details on all publications, add progressbars and finish statements

--- Cristin.py
+++ Cristin.py
@@ -7,21 +7,22 @@ from pandas.io.json import json_normalize
 ### Set desired institution ###
 institution = 7548
 
+### Send initial API call, fetch, flatten and store first set of data ###
+print("Sending initial API call...")
 done = False
-counter = 1
-thisPage = requests.get(f"https://api.cristin.no/v2/results?institution={institution}&per_page=1000")
-sys.stdout.write("Working")
+thisPage = requests.get(f"https://api.cristin.no/v2/results?institution={institution}&per_page=200")
+sys.stdout.write("Gathering all relevant data")
 totalHits = thisPage.headers["X-Total-Count"]
-
 flatData = json_normalize(json.loads(thisPage.text))
 cleanData = flatData[["url"]]
 
+    
+### Loop through all relevant pages of API, fetch, flatten, clean and store data ###
 while not done:
     nextPage = requests.get(thisPage.links["next"]["url"])
     try:
         if nextPage.links["next"] is not None:
             thisPage = nextPage
-            counter = counter + 1
             sys.stdout.flush()
             sys.stdout.write(".")
             flatData = flatData.append(json_normalize(json.loads(thisPage.text)), sort=True)
@@ -29,19 +30,30 @@ while not done:
     except KeyError:
         flatData = flatData.append(json_normalize(json.loads(nextPage.text)), sort=True)
         cleanData = flatData[["url"]]
-        counter = counter + 1
         sys.stdout.write(" Done!")
         done = True
 
 ### Check if all data is gathered ###
-totalHits = int(totalHits) + 1
 if len(cleanData.index) == int(totalHits):
     print("\n=== All data gathered ===")
     
-    ### Save to Publications.xlsx ###
-    cleanData.to_excel("Publications.xlsx", index=False)
-    print("=== Publications.xlsx written ===")
+    ### Write to ResultURLs.xlsx ###
+    cleanData.to_excel("ResultURLs.xlsx", index=False)
+    print("=== Writing ResultURLs.xlsx ===")
+    print("=== ResultURLs.xlsx complete ===")
+
+    ### Write all publication details to Publications.xlsx ###
+    flatDetails = pd.DataFrame()
+    print("=== Writing Publications.xlsx ===")
+    for index,url in enumerate(cleanData["url"]):
+        print(f'{index} of {totalHits} publications written\r', end="")
+        details = requests.get(str(url))
+        flatDetails = flatDetails.append(json_normalize(json.loads(details.text)), sort=True)
+    print()
+    flatDetails.to_excel("Publications.xlsx", index=False)
+    print("=== Publications.xlsx complete ===")
 else:
         print("\n### ERROR WHILE FETCHING DATA ###")
+        print("### NO DATA WRITTEN TO FILE ###")
 
 
